8f45e6209d0e560247d1b861edf3c56a0e927fd0
Shmurkio <112756303+Shmurkio@users.noreply.github.com>
2026-06-15 19:36:46 +0200

    Sample application

--- UEFIpp/Entry.cpp
+++ UEFIpp/Entry.cpp
@@ -2,18 +2,32 @@
 
 auto Main([[maybe_unused]] const Vector<String>& Args) -> Foundation::Bool
 {
-    Stream::Out::Console.Clear();
+    // Argument parsing and serial output stream
+    Stream::Out::Serial << "Argument count: " << Args.Size() << Stream::Endl;
 
-    Stream::Out::Console << "Press any key to continue.." << Stream::Endl;
+    Foundation::Size Index{};
+
+    for (const auto& Arg : Args)
+    {
+        Stream::Out::Serial << "  [" << Index++ << "] " << Arg << Stream::Endl;
+    }
+
+    // Console output stream
+    Stream::Out::Console << "UEFI++ Sample Application" << Stream::Endl << Stream::Endl;
+
+    // Console input stream
+    Stream::Out::Console << "Press any key to continue..." << Stream::Endl;
+
     Stream::In::Console.Get();
 
-    Stream::Out::Console << "Hello, UEFI!" << Stream::Endl;
+    Stream::Out::Console << "We love UEFI!" << Stream::Endl;
 
     String Name{};
     Stream::Out::Console << "Enter name: " << Stream::FlushNow;
     Stream::In::Console >> Name;
 
-    Stream::Out::Console << "Hello, " << Name << Stream::Endl;
+    Stream::Out::Console << "Hello, " << Name << "!"  << Stream::Endl;
+    Stream::Out::Serial << "Entered name: " << Name << Stream::Endl;
 
     return true;
 }
